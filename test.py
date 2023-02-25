d38836ae57f593ab14a531b002c60fefaa2a66b4
Jhin-Hsing <elf90121@gmail.com>
2023-02-25 18:48:13 +0800

    把部分函式分離出去myFunc.py、新增fitness_evaluate()

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
 import pandas as pd
 
-order = pd.read_excel('製令單_1121-1125 - 少.xlsx')
+a = [2,3,5,1,-2]
 
-new_index = [10, 4, 9, 2, 16]
-
-df = order.reindex(new_index)
-
-
-ts1 = df['預計出貨'][10]
-ts2 = pd.to_datetime('2022/11/30', format='%Y/%m/%d')
-
-print(ts1)
-print(ts2)
-
-print(ts1<ts2)
+print(min(a))
