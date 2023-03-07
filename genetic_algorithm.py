973a9cd9bd7eb98dae2c24891aa289c6ac26f144
Jhin-Hsing <elf90121@gmail.com>
2023-03-07 16:43:27 +0800

    更新交配、突變方式

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -2,6 +2,7 @@
 genetic algorithm for a scheduling problem
 
 '''
+from datetime import datetime
 import matplotlib.pyplot as plt
 import time
 import random
@@ -120,6 +121,77 @@ def check_feasibility(individual,order,manuTable,lineTable,dailySheet,fillTable_
 
 
     #檢查是否有超過交期
+    #前者為True才需要檢查
+    if test1:
+        crew_num = 1
+        for df in df_schedule:
+            if not test2:break
+            for idx in df.index:
+                complete = pd.to_datetime(df['預計完工'][idx], format='%Y/%m/%d')
+
+                due = df['預計出貨'][idx]
+                if complete > due:
+                    # print(str(crew_num)+'班',idx,complete,due)
+                    test2 = False
+                    # quit()
+                    break
+            crew_num += 1
+
+    result = test1 and test2
+
+    return result
+
+#可行解檢查(final debug)
+def check_feasibility2(individual,order,manuTable,lineTable,dailySheet,fillTable_path):
+    '''
+    (a) 該工班能不能做這張單
+    (b) 做完會不會超過交期
+
+
+    (a) test1 :
+        1.簡單解碼後去逐個比對，只要發現一筆違反限制就break
+
+    (b) test2:
+        1.將完整製令單依照各班排程表切分成df並放入df_schedule
+        2.傳入calculating_done 計算完工時間
+        3.確認是否所有訂單都沒有逾期 (完工日>出貨日)，一樣只要發現一筆就break
+
+    '''
+    result = True
+    test1 = True
+    test2 = True
+    schedule = easyDecode(individual)
+    df_schedule = []
+
+    #依序確認工班可不可以做該類型
+    crew_num = 1
+    for lst in schedule:
+        if not test1:break
+
+        for idx in lst:
+            type = order['類型'][idx]
+            if manuTable[f'製{crew_num}班11人'][type]==0:
+                # print(individual)
+                print(f'{idx}: 生產限制: {type},無法於{crew_num}班加工')
+
+                df.to_excel('./debug/vio.xlsx')
+                test1 = False
+        crew_num += 1
+
+    # print(schedule)
+    # print(test1)
+    # quit()
+
+    #計算各班訂單完工時間
+    df_schedule = convert_to_dataFrame(schedule,order)
+    crew_num = 1
+    for df in df_schedule:
+        df = calculating_done(str(crew_num)+'班',df,lineTable,dailySheet,fillTable_path)
+        # df.to_excel(f'./debug/{crew_num}.xlsx')
+        crew_num += 1
+
+
+    #檢查是否有超過交期
     crew_num = 1
     for df in df_schedule:
         if not test2:break
@@ -128,13 +200,17 @@ def check_feasibility(individual,order,manuTable,lineTable,dailySheet,fillTable_
 
             due = df['預計出貨'][idx]
             if complete > due:
-                # print(str(crew_num)+'班',idx,complete,due)
+                # print(individual)
+                print(f'{crew_num}班 訂單{idx} 超過交期 預計完工:{complete} 預計出貨:{due}')
                 test2 = False
-                # quit()
+
+                df.to_excel('./debug/vio.xlsx')
                 break
         crew_num += 1
 
     result = test1 and test2
+
+
 
     return result
 
@@ -293,72 +369,168 @@ def selection(population,order,lineTable):
 
     return mating_pool
 
-#選擇(菁英法)
-def selection_elite(population,order,lineTable):
-    mating_pool = []
+#將染色體依照交貨日期排序
+def sort_by_dueDay(individual,order):
 
+    # 解碼為部分排程表DF
+    df_schedule = convert_to_dataFrame(easyDecode(individual),order)
 
-    return mating_pool
+    crew_num = 1
+    new_individual = []
+    for df in df_schedule:
 
+        # 直接以 time stamp 排序
+        df.sort_values('預計出貨',inplace=True)
+
+        # 新染色體加入工班代號
+        new_individual.append('X'+str(crew_num))
+        crew_num += 1
+
+        # 新染色體加入排序後的編號順序
+        new_individual.extend(list(df.index))
+
+    return new_individual
 
 #交配
-def crossover(parent1,parent2):
-    '''
-    交配
+def crossover(p1,p2):
+    child = []
 
-    傳入兩個個體，回傳子代
-    1.產生突變點 cut point
-    2.保留 parent1 從頭到 cut point 的基因
-    3.剩餘部分由 parent2 以"不跟前面重複"的方式取出基因補齊
-    (*)4.以此類推產生 child2
+    # 找出每個'X'的位置
+    p1_idx1 = p1.index('X2')
+    p1_idx2 = p1.index('X4')
 
-    '''
-    # 產生交配點
-    cut_point = random.randint(0,len(parent1)-1)
+    p2_idx1 = p2.index('X2')
+    p2_idx2 = p2.index('X4')
 
-    # 產生 child1
-    child1 = parent1[:cut_point]
-    for gene in parent2:
-        if gene not in child1:
-            child1.append(gene)
+    # 切片取出每個分段
+    p1_seg1 = p1[:p1_idx1]
+    p1_seg2 = p1[p1_idx1:p1_idx2]
+    p1_seg3 = p1[p1_idx2:]
 
-    # # 產生 child2
-    # child2 = parent2[:cut_point]
-    # for gene in parent1:
-    #     if gene not in child2:
-    #         child2.append(gene)
+    p2_seg1 = p2[:p2_idx1]
+    p2_seg2 = p2[p2_idx1:p2_idx2]
+    p2_seg3 = p2[p2_idx2:]
 
-    return child1
+    # 分段交配
+    def seg_cross(s1,s2):
+
+        child_seg = []
+
+        # 找到比較短的長度
+        seg_len = len(s1) if len(s1) < len(s2) else len(s2)
+
+        # 產生交配點
+        cut_point = random.randint(0,seg_len-1)
+
+        # 進行交配
+        child_seg = s1[:cut_point]
+
+        for gene in s2:
+            if gene not in child_seg:
+                child_seg.append(gene)
+
+        return child_seg
+
+    # 刪除重複數字
+    def keepOne(original_list):
+        new_list = []
+        appeared_set = set()
+
+        for num in original_list:
+
+            # 數字尚未出現過
+            if num not in appeared_set:
+                new_list.append(num)
+                appeared_set.add(num)
+
+            # 數字已經存在
+            else:
+                if random.random() < 0.5:
+
+                    # 把原本的數字刪掉再加入，讓他跑到最後面的位置
+                    new_list.remove(num)
+                    new_list.append(num)
+
+        return new_list
+
+    child_seg1 = seg_cross(p1_seg1,p2_seg1)
+    child_seg2 = seg_cross(p1_seg2,p2_seg2)
+    child_seg3 = seg_cross(p1_seg3,p2_seg3)
+
+    child = child_seg1 + child_seg2 + child_seg3
+    child = keepOne(child)
+
+    return child
 
 #突變
 def mutation(individual):
     '''
     突變
-    1.產生兩個突變點，突變點位置"不能"是工班代號，即Xn
-    2.交換兩個位置的基因
+    1.同個工班內各自突變
+    2.產生兩個突變點，突變點位置"不能"是工班代號，即Xn
+    3.交換兩個位置的基因
 
     '''
-    muta_p1 = random.randint(1,len(individual)-1)
-    while(type(individual[muta_p1])!=int):
-        muta_p1 = random.randint(1,len(individual)-1)
+    # 解碼為二維陣列
+    schedule = easyDecode(individual)
 
+    # 每個工班各自產生突變點並突變
+    for crew_list in schedule:
 
-    muta_p2 = random.randint(1,len(individual)-1)
-    while(type(individual[muta_p2])!=int):
-        muta_p2 = random.randint(1,len(individual)-1)
+        # 如果這班只做一張訂單則跳過
+        if len(crew_list)==1:continue
 
-    individual[muta_p1],individual[muta_p2] = \
-        individual[muta_p2],individual[muta_p1]
+        # 產生突變點1
+        muta_p1 = random.randint(1,len(crew_list)-1)
+        while(type(crew_list[muta_p1])!=int):
+            muta_p1 = random.randint(1,len(crew_list)-1)
 
-    return individual
+        # 產生突變點2
+        muta_p2 = random.randint(1,len(crew_list)-1)
+        while(type(crew_list[muta_p2])!=int):
+            muta_p2 = random.randint(1,len(crew_list)-1)
+
+        # 交換兩突變點中的基因
+        crew_list[muta_p1],crew_list[muta_p2] = \
+            crew_list[muta_p2],crew_list[muta_p1]
+
+    # 重新編碼回染色體
+    crew_num =  1
+    new_individual = []
+    for s in schedule:
+        new_individual.append('X'+str(crew_num))
+        crew_num += 1
+        new_individual.extend(s)
+
+    return new_individual
+
+#繪製圖表
+def draw(lst):
+
+    # 以 fitness 繪製
+    plt.plot(lst)
+    plt.xlabel('generation')
+    plt.ylabel('fitness')
+    plt.savefig('./output/fitness_per_generation.jpg')
+    plt.clf()
+
+    # 轉換回setup time
+    lst = [1/x for x in lst]
+
+    # 以 setup time 繪製
+    plt.plot(lst)
+    plt.xlabel('generation')
+    plt.ylabel('setup time')
+    plt.savefig('./output/setup_time_per_generation.jpg')
+    plt.clf()
 
 #主程式
 def main():
 
     POPULATION_SIZE = 40
-    MAX_GENERATION = 100
+    MAX_GENERATION = 200
     CROSSOVER_RATE = 0.8
-    MUTATION_RATE = 0.05
+    MUTATION_RATE = 0
     LOST = [3,3,3,3]
 
     '''
@@ -376,7 +548,7 @@ def main():
     print('讀取製程資訊...')
     fillTable_path = './input_data/待填工時表單-20221121-2-2-2-3.xlsx'
     typeTable_path = './input_data/福佑電機製造部工時總攬資料_V3.xlsx'
-    order = pd.read_excel('./input_data/製令單_1121-1125 - 少.xlsx')
+    order = pd.read_excel('./input_data/製令單_1121-1125.xlsx')
     typeTable = pd.read_excel(typeTable_path,skiprows=1)
     dailySheet = generate_dailySheet(fillTable_path,LOST)
 
@@ -395,7 +567,6 @@ def main():
 
     print('訂單筆數:',len(order))
 
-
     '''
     初始化族群
 
@@ -403,66 +574,66 @@ def main():
     2. 檢查個體是否為可行解，如果不是，重複步驟1，直到生成可行解。
     3. 儲存可行解到population列表中。
     4. 重複步驟1~3，直到population列表中有足夠的個體(即POPULATION_SIZE)。
+    5. 將所有個體基因依交貨日進行排序
 
     '''
+    print(datetime.now().strftime("%H:%M:%S"))
     print(f'\n[{round(time.process_time(),2)}s] 初始化族群')
 
     population = []
     individual_num = 1
     for i in range(POPULATION_SIZE):
 
+        # 維持可行解
         individual = init_individual(order,manuTable)
         while(not check_feasibility(individual,order,manuTable,lineTable,dailySheet,fillTable_path)):
             individual = init_individual(order,manuTable)
 
-        print(f'產生第{individual_num}個可行解')
-
         individual_num += 1
+
+        # 依照出貨日進行排序
+        individual = sort_by_dueDay(individual,order)
+
+        # 加入族群
         population.append(individual)
 
-    # for individual in population:
-    #     print(individual)
-    # quit()
 
     '''
-    演算法演化
+    演化
 
     1. 使用一個迴圈重複執行 MAX_GENERATION 次。
     2. 在每一次迴圈中，先計算當前 population 中各個體的適應值
     3. 使用 selection 函式，選擇並產生配對池。
-    4. 使用 crossover 函式，在配對池中進行交配，產生子代族群 offspring。
-    5. 將 offspring 取代當前 population ，完成一次世代演化。
-    6. 重複執行第 2~5 步，直到執行 MAX_GENERATION 次為止。
+    4. 從配對池中取出一對個體進行交配，產生子代個體 child1、child2。
+    5. 將子代基因依照出貨日期排序
+    6. 重複4.~5. 直到達到族群上限，產生子代族群 offspring
+    7. 重複執行第 2~6 步，直到執行 MAX_GENERATION 次為止。
 
     '''
 
     # 紀錄每代最佳 fitness
     best_fitness_list = []
 
-    # 設定計時器，避免迭代時間過久
-    start_time = time.time()
-
-    # 設定迭代時間上限
-    time_limit = 1200
-
-    print(f'\n[{round(time.process_time(),2)}s] 開始演化世代')
+    print('\n',datetime.now().strftime("%H:%M:%S"))
+    print(f'[{round(time.process_time(),2)}s] 開始演化世代')
     for generation in range(MAX_GENERATION):
 
-        # 確認是否超時
-        if time.time() - start_time > time_limit:break
-
-        # print('計算適應度...')
+        # 儲存族群所有個體的 fitness
         fitness_list = []
+
+        # 計算所有個體的 fitness
         for individual in population:
             fitness = fitness_evaluate(individual,order,lineTable)
             fitness_list.append(fitness)
 
+        # 取出此世代中最好的 fitness
         best_fitness = max(fitness_list)
 
+        # 將最好 fitness 加入 best_fitness_list
         best_fitness_list.append(best_fitness)
         print(f'generation:{generation+1} best fitness = {best_fitness}')
 
-        #seletion
+        # 使用 selection 建立配對池
         mating_pool = selection(population,order,lineTable)
 
         '''
@@ -474,30 +645,32 @@ def main():
             小於於突變率:進行交配產生兩個 child 個體
 
         '''
+
         # 建立子代族群 offspring
         offspring = []
+
+        # 遍歷 mating_pool，一次取出一對個體
         for i in range(0,POPULATION_SIZE,2):
 
             # 取出父母
             parent1,parent2 = mating_pool[i],mating_pool[i+1]
 
+            # 產生隨機值，並比較交配率
             if random.random() < CROSSOVER_RATE:
 
+                # 進行交配，產生個體 child1、child2
                 child1 = crossover(parent1,parent2)
                 child2 = crossover(parent2,parent1)
 
-                # 維持可行解
-                while(not check_feasibility(child1,order,manuTable,lineTable,dailySheet,fillTable_path)):
-                    child1 = crossover(parent1,parent2)
-
-                while(not check_feasibility(child2,order,manuTable,lineTable,dailySheet,fillTable_path)):
-                    child2 = crossover(parent2,parent1)
+                # 依照出貨日期進行排序
+                child1 = sort_by_dueDay(child1,order)
+                child2 = sort_by_dueDay(child2,order)
 
             else:
                 # 直接複製父母
                 child1,child2 = parent1,parent2
 
-            # 加入 offspring
+            # 將子代個體加入 offspring
             offspring.extend([child1,child2])
 
         '''
@@ -505,37 +678,39 @@ def main():
         1.遍歷整個族群並產生隨機值，低於突變率進行突變操作
 
         '''
+        # 遍歷子代族群
         for i in range(len(offspring)):
+
+            # 產生隨機值並比較突變率
             if random.random() < MUTATION_RATE:
+
+                # 取出個體
                 individual = offspring[i]
 
+                # 進行突變
                 individual = mutation(individual)
 
-                # 維持可行解
-                # while(not check_feasibility(individual,order,manuTable,lineTable,dailySheet,fillTable_path)):
-                #     individual = mutation(individual)
-
-                # offspring[i] = individual
+                # 放回 offspring
+                offspring[i] = individual
 
         # 取代族群
         population = offspring
+
+        # 輸出圖表
+        draw(best_fitness_list)
 
 
     print('可行解檢查...')
     feasib_ok = []
     for i in range(len(population)):
         individual = population[i]
-        if check_feasibility(individual,order,manuTable,lineTable,dailySheet,fillTable_path):
+        if check_feasibility2(individual,order,manuTable,lineTable,dailySheet,fillTable_path):
            feasib_ok.append(individual)
     for individual in feasib_ok:
         print(individual)
     print(f"族群總數: {POPULATION_SIZE}  可行解: {len(feasib_ok)}")
 
-    print('繪製圖表...')
-    plt.plot(best_fitness_list)
-    plt.xlabel('generation')
-    plt.ylabel('fitness')
-    plt.savefig('./output/fitness_per_generation.jpg')
+
 
 
     print(f'\n[{round(time.process_time(),2)}s] 結束')
--- test.py
+++ test.py
@@ -1,44 +1,97 @@
 import random
-import numpy as np
+from genetic_algorithm import easyDecode,check_feasibility2
+p1 = ['X1', 16, 2, 9, 5, 14,
+      'X2', 0, 15, 13, 4, 8, 11, 3,
+      'X3', 12, 17, 7, 18, 23, 6, 10, 22, 1, 20, 19,
+      'X4', 21, 24]
 
-# fitness_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-
-# random.shuffle(fitness_values)
-
-# sum_fitness = sum(fitness_values)
-
-# prob_list = [value/sum_fitness for value in fitness_values]
-
-# add_prob_list = [sum(prob_list[:i+1]) for i in range(len(fitness_values))]
-
-
-# for i in range(len(fitness_values)):
-#     fit = round(fitness_values[i],2)
-#     prob = round(prob_list[i],2)
-#     add = round(add_prob_list[i],2)
-#     print(f'fitness:{fit}\t佔比:{prob}\t累積機率:{add}')
-# print(f"總和:{sum_fitness}\t總和:{sum(prob_list)}")
+p2 = ['X1', 15, 2,
+      'X2', 3, 18, 16, 13, 8, 11, 4, 10, 1,
+      'X3', 12, 6, 23, 22, 14, 20, 9, 0, 7, 17, 5,
+      'X4', 19, 24, 21]
 
 
-# max = sum([c.fitness for c in population])
-# selection_probs = [c.fitness/max for c in population]
-# return population[npr.choice(len(population), p=selection_probs)]
+
+'''
+用分段交配
+1班 x 1班
+2、3班 x 2、3班
+4班 x 4班
+
+1. 迴圈遍歷所有工班
+2. 找出p1 p2中較短的分段，從中產生一個隨機切點
+'''
+def crossover_new(p1,p2):
+    child = []
+
+    # 找出每個'X'的位置
+    p1_idx1 = p1.index('X2')
+    p1_idx2 = p1.index('X4')
+
+    p2_idx1 = p2.index('X2')
+    p2_idx2 = p2.index('X4')
+
+    # 切片取出每個分段
+    p1_seg1 = p1[:p1_idx1]
+    p1_seg2 = p1[p1_idx1:p1_idx2]
+    p1_seg3 = p1[p1_idx2:]
+
+    p2_seg1 = p2[:p2_idx1]
+    p2_seg2 = p2[p2_idx1:p2_idx2]
+    p2_seg3 = p2[p2_idx2:]
+
+    # 分段交配
+    def seg_cross(s1,s2):
+
+        child_seg = []
+
+        # 找到比較短的長度
+        seg_len = len(s1) if len(s1) < len(s2) else len(s2)
+
+        # 產生交配點
+        cut_point = random.randint(0,seg_len-1)
+
+        # 進行交配
+        child_seg = s1[:cut_point]
+
+        for gene in s2:
+            if gene not in child_seg:
+                child_seg.append(gene)
+
+        return child_seg
+
+    # 刪除重複數字
+    def keepOne(original_list):
+        new_list = []
+        appeared_set = set()
+
+        for num in original_list:
+
+            # 數字尚未出現過
+            if num not in appeared_set:
+                new_list.append(num)
+                appeared_set.add(num)
+
+            # 數字已經存在
+            else:
+                if random.random() < 0.5:
+
+                    # 把原本的數字刪掉再加入，讓他跑到最後面的位置
+                    new_list.remove(num)
+                    new_list.append(num)
+
+        return new_list
+
+    child_seg1 = seg_cross(p1_seg1,p2_seg1)
+    child_seg2 = seg_cross(p1_seg2,p2_seg2)
+    child_seg3 = seg_cross(p1_seg3,p2_seg3)
+
+    child = child_seg1 + child_seg2 + child_seg3
+    child = keepOne(child)
+
+    return child
 
 
-# import matplotlib.pyplot as plt
 
-# import matplotlib.pyplot as plt
 
-# # 兩個陣列
-# A = [1, 2, 3, 4, 5]
-# B = [2, 4, 6, 8, 10]
-
-# # 繪圖
-# plt.plot(A, label='Line A')
-# plt.plot(B, label='Line B')
-
-# # 圖例
-# plt.legend()
-
-# # 顯示圖形
-# plt.show()
+# p3,p4 = easyDecode(p3),easyDecode(p4)
